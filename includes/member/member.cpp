1bfd1c074552605fd4b016aa2b989069ad7e36ee
Leon Hu <jhu27@go.pasadena.edu>
2022-02-23 23:30:12 -0800

    member class

--- includes/member/member.cpp
+++ includes/member/member.cpp
@@ -1,80 +1,184 @@
 
 #include "member.h"
 
-member::member() {
+member::member()
+{
+    //    member_name="";
+    //    member_id ="";
+    //    member_type = "";
+    //    member_expired_date = "";
 }
 
-member::member(string name, string id):member_name(name),member_id(id) {
-
-}
-const string &member::getMemberName() const {
-    return member_name;
+member::member(string name, string id, string type, string date) : member_name(name), member_id(id), member_type(type), member_expired_date(date)
+{
 }
 
-void member::setMemberName(const string &memberName) {
-    member_name = memberName;
-}
-
-const string &member::getMemberId() const {
-    return member_id;
-}
-
-void member::setMemberId(const string &memberId) {
-    member_id = memberId;
-}
-
-const string &member::getMemberExpiredDate() const {
-    return member_expired_date;
-}
-
-void member::setMemberExpiredDate(const string &memberExpiredDate) {
-    member_expired_date = memberExpiredDate;
-}
-
-const string &member::getMemberIssueDate() const {
-    return member_issue_date;
-}
-
-void member::setMemberIssueDate(const string &memberIssueDate) {
-    member_issue_date = memberIssueDate;
-}
-
-int member::getYearlyRebate() const {
-    return yearly_rebate;
-}
-
-void member::setYearlyRebate(int yearlyRebate) {
-    yearly_rebate = yearlyRebate;
-}
-
-
-void member::setMemberType(int memberPayment) {
-    if(memberPayment == BASIC_MEMBER_ANNUAL){
+void member::setMemberType(int memberPayment)
+{
+    if (memberPayment == BASIC_MEMBER_ANNUAL)
+    {
         member_type = "BASIC";
-        yearly_rebate =0;
-        //setMemberIssueDate()
+        yearly_rebate = 0;
+
+        vector<tm *> date = getCurrentDate();
+        string date_string = "";
+        for (int i = 0; i < date.size(); i++)
+        {
+            date_string += to_string(reinterpret_cast<int>(date.at(i)));
+        }
+        setMemberIssueDate(date_string);
+
+        // set expired date, one year period
+        // setMemberExpiredDate(get)
     }
-    else if(memberPayment == PREFE_MEMBER_ANNUAL){
+    else if (memberPayment == PREFE_MEMBER_ANNUAL)
+    {
         member_type = "PREFERRED";
         yearly_rebate = 0.05;
     }
-    else{
+    else
+    {
         cout << "Invalid member payment\n";
     }
 }
 
-const vector<member> &member::getMemberList() const {
-    return member_list;
+void member::read_file(string file_name)
+{
+
+    ifstream in(file_name.c_str());
+    if (in.fail())
+    {
+        cout << "Open file failed.\n";
+        exit(1);
+    }
+    string name, id, type, date;
+
+    member m;
+
+    do
+    {
+        // in >> name;
+        getline(in, name);
+        getline(in, id);
+        getline(in, type);
+        getline(in, date);
+
+        m.set_info(name, id, type, date);
+        // member_list.push(m);
+        // repo.add_member(m);
+        m.print();
+
+    } while (!in.eof());
+
+    in.close();
 }
 
-void member::setMemberList(member each_member) {
-    member_list.push_back(each_member);
+void member::set_info(string name, string id, string type, string date)
+{
+    setMemberName(name);
+    setMemberId(id);
+    setMemberType(type);
+    setMemberExpiredDate(date);
+}
+// const LinkedList<member> &member::getMemberList() const {
+//     return member_list;
+// }
+
+void member::print()
+{
+
+    cout << "Shopper: [" << this->getMemberName() << "], [" << this->getMemberId() << "], ["
+         << this->getMemberType() << "], [" << this->getMemberExpiredDate() << "]\n";
+}
+// void member::print_list() {
+//
+//     for(int i=0; i< member_list.listSize(); i++){
+//         cout << member_list;
+//         cout << __LINE__<< endl;
+//     }
+//     cout << __LINE__<< endl;
+// }
+
+vector<tm *> member::getCurrentDate()
+{
+    time_t t = time(NULL);
+    tm *timePtr = localtime(&t);
+
+    vector<tm *> temp;
+    temp.push_back(reinterpret_cast<tm *const>(timePtr->tm_year + 1900));
+    temp.push_back(reinterpret_cast<tm *const>(timePtr->tm_mon + 1));
+    temp.push_back(reinterpret_cast<tm *const>(timePtr->tm_mday));
+
+    //    string year = to_string(timePtr->tm_year +1900);
+    //    string month = to_string(timePtr->tm_mon +1);
+    //    string day = to_string(timePtr->tm_mday);
+
+    return temp;
 }
 
-const string &member::getMemberType() const {
+void member::setPurchaseHistory(string item)
+{
+}
+
+vector<string> member::getPurchaseHistory()
+{
+}
+const string &member::getMemberType() const
+{
     return member_type;
 }
 
-void member::setMemberType(const string &memberType) {
+void member::setMemberType(const string &memberType)
+{
     member_type = memberType;
+}
+const string &member::getMemberName() const
+{
+    return member_name;
+}
+
+void member::setMemberName(const string &memberName)
+{
+    member_name = memberName;
+}
+
+const string &member::getMemberId() const
+{
+    return member_id;
+}
+
+void member::setMemberId(const string &memberId)
+{
+    member_id = memberId;
+}
+
+const string &member::getMemberExpiredDate() const
+{
+    return member_expired_date;
+}
+
+void member::setMemberExpiredDate(const string &memberExpiredDate)
+{
+    member_expired_date = memberExpiredDate;
+}
+
+const string &member::getMemberIssueDate() const
+{
+    return member_issue_date;
+}
+
+void member::setMemberIssueDate(const string &memberIssueDate)
+{
+    member_issue_date = memberIssueDate;
+}
+
+double member::getYearlyRebate() const
+{
+    return yearly_rebate;
+}
+
+void member::setYearlyRebate(int total_amount)
+{
+    // if()
+    yearly_rebate = total_amount * 0.05;
 }
--- includes/member/member.h
+++ includes/member/member.h
@@ -5,45 +5,57 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <ctime>
+#include <map>
 
 using namespace std;
-class member {
-
+class member
+{
 
 public:
+    const int BASIC_MEMBER_ANNUAL = 60;
+    const int PREFE_MEMBER_ANNUAL = 75;
+
+    // report repo;
     member();
-    member(string name, string id);
+    member(string name, string id, string type, string date);
 
-    vector<member> member_list;
+    // LinkedList<member> member_list;
+    vector<tm *> getCurrentDate();
+    void setPurchaseHistory(string item);
+    vector<string> getPurchaseHistory();
 
-    void setMemberList(member ech_member);
-    const vector<member> &getMemberList() const;
-    const string &getMemberName() const;
-    void setMemberName(const string &memberName);
-    const string &getMemberId() const;
-    void setMemberId(const string &memberId);
-    const string &getMemberExpiredDate() const;
-    void setMemberExpiredDate(const string &memberExpiredDate);
-    const string &getMemberIssueDate() const;
-    void setMemberIssueDate(const string &memberIssueDate);
-    int getYearlyRebate() const;
-    void setYearlyRebate(int yearlyRebate);
+    void print();
+    // void print_list();
+    void read_file(string file_name);
+    void set_info(string name, string id, string type, string date);
+    // void add_member(member member1);
+
+    // const LinkedList<member> &getMemberList() const;                    //getter for list of member
+    const string &getMemberName() const;                        // getter of member name
+    void setMemberName(const string &memberName);               // set member name
+    const string &getMemberId() const;                          // getter of member id
+    void setMemberId(const string &memberId);                   // set member id
+    const string &getMemberExpiredDate() const;                 // getter of member expired date
+    void setMemberExpiredDate(const string &memberExpiredDate); // set member expired date
+    const string &getMemberIssueDate() const;                   // getter of member issue date
+    void setMemberIssueDate(const string &memberIssueDate);     // set member issue date
+    double getYearlyRebate() const;                             // getter of member yearly rebate
+    void setYearlyRebate(int total_amount);
     void setMemberType(int memberPayment);
     const string &getMemberType() const;
     void setMemberType(const string &memberType);
 
-
-    const int BASIC_MEMBER_ANNUAL = 60;
-    const int PREFE_MEMBER_ANNUAL = 75;
 private:
     string member_name;
     string member_id;
-    string member_type;         //basic / preferred
+    string member_type; // basic / preferred
     string member_expired_date;
     string member_issue_date;
-    double yearly_rebate;      //5% FOR PREFERR MEMBER
-
+    double yearly_rebate; // 5% FOR PREFERR MEMBER
+    double member_purchase;
+    vector<string> purchase_history;
 };
 
-
-#endif //PROJ_WAREHOUSE_MEMBER_H
+#endif // PROJ_WAREHOUSE_MEMBER_H
